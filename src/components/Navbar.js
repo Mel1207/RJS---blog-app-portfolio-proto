b41cc3cdcd66ca97c2f0339d86a4322a5a0fee02
Mel <meljohnpualon@gmail.com>
2021-10-07 06:33:23 +0800

    fix navbar styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,12 +4,17 @@ import { NavLink } from 'react-router-dom'
 const Navbar = () => {
     return (
         <header className="bg-red-500 w-100 d-flex justify-around text-white">
-            <div className="container mx-auto flex justify-between">
-                <nav>
-                    <NavLink to="/" exact>MJP</NavLink>
-                    <NavLink to="/post">Blog Post</NavLink>
-                    <NavLink to="/project">Projects</NavLink>
-                    <NavLink to="about">About Me</NavLink>
+            <div className="container mx-auto">
+                <nav className="flex item-center justify-between py-5">
+                    <NavLink to="/" exact className="cursive" activeClassName="text-red">MJP</NavLink>
+                    <div>
+                        <NavLink to="/post" className="mx-3">Blog Post</NavLink>
+                        <NavLink to="/project" className="mx-3">Projects</NavLink>
+                        <NavLink to="about" className="mx-3">About Me</NavLink>
+                    </div>
+                    <div>
+                        social icons
+                    </div>
                 </nav>
             </div>
         </header>
