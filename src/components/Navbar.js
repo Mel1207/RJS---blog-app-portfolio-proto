ab7541ff05c82d6a9b0a4e33d4ded8492c66524a
Mel <meljohnpualon@gmail.com>
2021-10-07 06:13:27 +0800

    added basic tailwind style on navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
     return (
-        <div className="navbar">
-            
-        </div>
+        <header className="bg-red-500 w-100 d-flex justify-around text-white">
+            <div className="container mx-auto flex justify-between">
+                <nav>
+                    <NavLink to="/" exact>MJP</NavLink>
+                    <NavLink to="/post">Blog Post</NavLink>
+                    <NavLink to="/project">Projects</NavLink>
+                    <NavLink to="about">About Me</NavLink>
+                </nav>
+            </div>
+        </header>
     )
 }
 
