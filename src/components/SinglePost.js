99a77fe13944f7ed30630ee0f791a20fc8817f82
Mel <meljohnpualon@gmail.com>
2021-10-10 00:09:28 +0800

    added sanity image url

--- src/components/SinglePost.js
+++ src/components/SinglePost.js
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import sanityClient from '../client.js'
+import imageUrlBuilder from '@sanity/image-url'
 
 const SinglePost = () => {
+    const [ singlePost, setSinglePost ] = useState(null);
+    const { slug } = useParams();
+
+
     return (
         <h1>This is single post</h1>
     )
