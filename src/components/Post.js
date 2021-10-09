0326f8e1fd8a61fb47ccd6f6036e0e833a664519
Mel <meljohnpualon@gmail.com>
2021-10-09 16:40:25 +0800

    added blog post and fetch data

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom';
-import sanityClient from '../client'
+import sanityClient from '../client.js'
 
-const Post = () => {
+export default function Post() {
     const [postData, setPost] = useState(null);
 
 
@@ -17,29 +17,33 @@ const Post = () => {
                 },
                 alt
             }
-        }`).then((data) => setPost(data)).catch(console.error())
+        }`)
+        .then(data => setPost(data))
+        .catch(console.error())
     }, []);
+
+    // console.log(post.slug.current)
 
     return (
         <main className="bg-green-100 min-h-screen p-12">
             <section className="container mx-auto">
-                <h1>Blog Post Page</h1>
-                <h2>Welcome to my page of blog post</h2>
-                <div>
-                    <article>
-                        <Link>
-                            <span>
-                                <img src="" alt="" />
-                                <span>
-                                    <h3></h3>
+                <h1 className="text-5xl">Blog Post Page</h1>
+                <h2 className="text-lg">Welcome to my page of blog post</h2>
+                <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+                    {postData && postData.map((post, index) => (
+                        <article>
+                            <Link to={"/post/" + post.slug} key={post.slug}>
+                                <span className="block h-64 relative rounded shadow leading-snug bg-white border-l-8 border-green-400" key={index}>
+                                    <img src={post.mainImage.asset.url} alt={post.mainImage.alt} className="w-full h-full rounder-r object-cover absolute" />
+                                    <span className="block relative h-full flex justify-end items-end pr-4 pb-4">
+                                        <h3 className="text-gray-800 text-lg px-3 py-4 bg-red-700 font-bold text-red-100 bg-opacity-75">{post.title}</h3>
+                                    </span>
                                 </span>
-                            </span>
-                        </Link>
-                    </article>
+                            </Link>
+                        </article>
+                    ))}
                 </div>
             </section>
         </main>
     )
 }
-
-export default Post
