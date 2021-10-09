7244bf58996e71df090a510d5b31de186ab8a8b9
Mel <meljohnpualon@gmail.com>
2021-10-09 14:42:25 +0800

    styled home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,15 @@
 import React from 'react'
+import ImgBg from '../img/img-bg.jpg'
 
 const Home = () => {
     return (
         <main>
-            <img src="" alt="" />
-            <section>
-                <h1>Mel John Pualon</h1>
-                <h2>Front End Developer / UI/UX Designer</h2>
+            <img src={ImgBg} alt="" className="absolute object-cover w-full h-full"/>
+            <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8 header-text cursive ">
+                <div>
+                    <h1 className="font-bold">Mel John Pualon</h1>
+                    <h2>Front End Developer / UI/UX Designer</h2>
+                </div>
             </section>
         </main>
     )
